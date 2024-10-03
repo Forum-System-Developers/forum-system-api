08745dbfdc412c17b41eca5907a026f480c69a6d
Sph1nx <nikita.ryapok@proton.me>
2024-10-03 18:35:11 +0300

    Added new schema for conversation class Conversation Response class Conversation Messages Response

---
+++ forum_system_api/schemas/conversation.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+from sqlalchemy.dialects.postgresql import UUID
+from datetime import datetime
+
+
+class ConversationResponse(BaseModel):
+    id: UUID
+    user1_id: UUID
+    user2_id: UUID
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class ConversationMessagesResponse(BaseModel):
+    id: UUID
+    content: str
+    author_id: UUID
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
