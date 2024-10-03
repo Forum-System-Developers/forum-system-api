e391e75b8d58fc8d774e9316b9c04c5aa4636f54
Sph1nx <nikita.ryapok@proton.me>
2024-10-03 18:33:28 +0300

    Added new schema for message class MessageCreate class MessageResponse

---
+++ forum_system_api/schemas/message.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from sqlalchemy.dialects.postgresql import UUID
+from datetime import datetime
+
+
+class MessageCreate(BaseModel):
+    content: str
+    conversation_id: UUID
+
+    class Config:
+        orm_mode = True
+
+
+class MessageResponse(BaseModel):
+    id: UUID
+    content: str
+    author_id: UUID
+    conversation_id: UUID
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
