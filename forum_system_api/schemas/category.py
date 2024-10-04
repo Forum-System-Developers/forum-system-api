ee29ea0a542f6204cca21208d9907314f245a917
Sph1nx <nikita.ryapok@proton.me>
2024-10-04 19:17:43 +0300

    upd/Imports are formatted according to conventions

--- forum_system_api/schemas/category.py
+++ forum_system_api/schemas/category.py
@@ -1,18 +1,23 @@
 from datetime import datetime
-from pydantic import BaseModel
 from uuid import UUID
+
+from pydantic import BaseModel
+
 
 class CategoryBase(BaseModel):
     name: str
     is_private: bool = False
     is_locked: bool = False
 
+    class Config:
+        orm_mode = True
+
+
 class CreateCategory(CategoryBase):
-    created_at: datetime
+    pass
+
 
 class CategoryResponse(CategoryBase):
     id: UUID
     created_at: datetime
 
-    class Config:
-        orm_mode = True
