56731664428473d82d19a8dde1312d75a96e48a3
Sph1nx <nikita.ryapok@proton.me>
2024-10-04 19:26:17 +0300

    upd/Added new routes - conversation_router - message_router

--- forum_system_api/api/api_v1/api.py
+++ forum_system_api/api/api_v1/api.py
@@ -3,6 +3,8 @@ from fastapi import APIRouter
 from .routes.user_router import router as user_router
 from .routes.topic_router import topic_router
 from .routes.reply_router import reply_router
+from .routes.conversation_router import conversation_router
+from .routes.message_router import message_router
 
 
 api_router = APIRouter(prefix="/api/v1")
@@ -10,3 +12,5 @@ api_router = APIRouter(prefix="/api/v1")
 api_router.include_router(user_router)
 api_router.include_router(topic_router)
 api_router.include_router(reply_router)
+api_router.include_router(conversation_router)
+api_router.include_router(message_router)
