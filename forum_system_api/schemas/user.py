c830f4abb931caba08a5050570f069250eb6785d
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-10-04 13:45:46 +0300

    refactor: Remove validators.py and use constr for validation in user schema
    - Removed validators.py file.
    - Updated user schema models in user.py to use Pydantic's constr for validation.
    - Added validation constraints directly in UserBase model for username, first_name, last_name, and password fields.

--- forum_system_api/schemas/user.py
+++ forum_system_api/schemas/user.py
@@ -1,12 +1,10 @@
-from pydantic import BaseModel, EmailStr
-
-from .validators import Password, Username, Name
+from pydantic import BaseModel, EmailStr, constr
 
 
 class UserBase(BaseModel):
-    username: Username
-    first_name: Name
-    last_name: Name
+    username: constr(min_length=3, max_length=30, strip_whitespace=True, pattern=r'^[a-zA-Z0-9]+$') # type: ignore
+    first_name: constr(min_length=2, max_length=30, strip_whitespace=True, pattern=r'^[a-zA-Z]+$') # type: ignore 
+    last_name: constr(min_length=2, max_length=30, strip_whitespace=True, pattern=r'^[a-zA-Z]+$') # type: ignore
     email: EmailStr
 
     class Config:
@@ -14,7 +12,7 @@ class UserBase(BaseModel):
 
 
 class UserCreate(UserBase):
-    password: Password
+    password: constr(min_length=8, max_length=30, strip_whitespace=True) # type: ignore
 
 
 class UserResponse(UserBase):
