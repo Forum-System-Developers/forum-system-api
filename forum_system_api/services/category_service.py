9b35c6c4012a0bc8ea902e4fe14de1ff5972335e
Sph1nx <nikita.ryapok@proton.me>
2024-10-06 23:12:58 +0300

    Added these functions to category_service: feat: Implement create_category function to add a new category feat: Implement get_all function to retrieve all categories feat: Implement view_all_topics_in_category function to filter topics by category feat: Implement make_private_or_public function to toggle category privacy feat: Implement lock_or_unlock function to toggle category lock status

--- forum_system_api/services/category_service.py
+++ forum_system_api/services/category_service.py
@@ -1,12 +1,82 @@
 from uuid import UUID
 
 from sqlalchemy.orm import Session
+from fastapi import HTTPException
 
 from forum_system_api.persistence.models.category import Category
+from forum_system_api.persistence.models.topic import Topic
+from forum_system_api.schemas.topic import TopicResponse
+from forum_system_api.schemas.category import CreateCategory, CategoryResponse
+
+
+def create_category(data: CreateCategory, db: Session) -> CategoryResponse:
+    new_category = Category(**data.model_dump())
+    
+    db.add(new_category)
+    db.commit()
+    db.refresh(new_category)
+
+    return new_category
+
+
+def get_all(db: Session) -> list[CategoryResponse]:
+    categories = db.query(Category).all()
+
+    result = [
+            CategoryResponse(
+                id=category.id,
+                name=category.name,
+                is_private=category.is_private,
+                is_locked=category.is_locked,
+                created_at=category.created_at,
+                topic_count=len(category.topics)
+            )
+            for category in categories
+        ]
+
+    return result
+
+
+def view_all_topics_in_category(
+        category_id: UUID, 
+        all_topics: list[Topic]
+) -> list[TopicResponse]:
+    return [topic for topic in all_topics if topic.category_id == category_id]
 
 
 def get_by_id(category_id: UUID, db: Session) -> Category:
     return (db.query(Category)
                 .filter(Category.id == category_id)
                 .one_or_none())
+
+
+def make_private_or_public(
+        category_id: UUID, 
+        is_private: bool, 
+        db: Session
+) -> Category:
+    category = get_by_id(category_id, db)
+    if category is None:
+        raise HTTPException(status_code=404, detail="Category not found")
     
+    category.is_private = is_private
+    db.commit()
+    db.refresh(category)
+
+    return category
+
+
+def lock_or_unlock(
+        category_id: UUID, 
+        is_locked: bool, 
+        db: Session
+) -> Category:
+    category = get_by_id(category_id, db)
+    if category is None:
+        raise HTTPException(status_code=404, detail="Category not found")
+    
+    category.is_locked = is_locked
+    db.commit()
+    db.refresh(category)
+
+    return category
