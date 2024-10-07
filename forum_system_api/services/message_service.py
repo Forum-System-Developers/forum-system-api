89718a36229e29689470920edb66ee7e3bc4aa5a
Sph1nx <nikita.ryapok@proton.me>
2024-10-07 20:30:32 +0300

    Fix conversation logic in send_message - Updated conversation query to check for existing conversations   specifically between sender and receiver, ensuring a new conversation   is created only when no direct chat exists between them.

--- forum_system_api/services/message_service.py
+++ forum_system_api/services/message_service.py
@@ -1,24 +1,24 @@
-from uuid import UUID
-
 from sqlalchemy.orm import Session
 
-from ..persistence.models.conversation import Conversation
+from forum_system_api.persistence.models.user import User
 from ..persistence.models.message import Message
+from ..persistence.models.conversation import Conversation
 from ..schemas.message import MessageCreate
 
 
-def send_message(db: Session, message_data: MessageCreate, user_id: UUID) -> Message:
+def send_message(db: Session, message_data: MessageCreate, user: User) -> Message:
     conversation = db.query(Conversation).filter(
-        (Conversation.user1_id == user_id) | (Conversation.user2_id == user_id)
+        ((Conversation.user1_id == user.id) & (Conversation.user2_id == message_data.receiver_id)) |
+        ((Conversation.user1_id == message_data.receiver_id) & (Conversation.user2_id == user.id))
     ).first()
 
     if not conversation:
-        conversation = Conversation(user1_id=user_id, user2_id=message_data.receiver_id)
+        conversation = Conversation(user1_id=user.id, user2_id=message_data.receiver_id)
         db.add(conversation)
         db.commit()
         db.refresh(conversation)
 
-    message = Message(content=message_data.content, conversation_id=conversation.id, author_id=user_id)
+    message = Message(content=message_data.content, conversation_id=conversation.id, author_id=user.id)
     db.add(message)
     db.commit()
     db.refresh(message)
