002927df298f7e535ab31cf6e8379dcef80b47eb
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-10-17 22:12:53 +0300

    Add unit tests for user_service.get_by_id() function
    - Implemented test_getById_returnsUser_whenUserIsFound
    - Implemented test_getById_returnsNone_whenUserIsNotFound

--- tests/services/user_service_test.py
+++ tests/services/user_service_test.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import Session
 from forum_system_api.persistence.models.user import User
 from forum_system_api.services import user_service
 from tests.services.test_data import USER_1, USER_2
+from tests.services.utils import assert_filter_called_with
 
 
 class UserService_Should(unittest.TestCase):    
@@ -41,3 +42,31 @@ class UserService_Should(unittest.TestCase):
         self.assertListEqual(users, list())
         self.mock_db.query.assert_called_once_with(User)
         query_mock.all.assert_called_once()
+
+    def test_getById_returnsCorrect_whenUserIsFound(self) -> None:
+        # Arrange
+        query_mock = self.mock_db.query.return_value
+        filter_mock = query_mock.filter.return_value
+        filter_mock.first.return_value = self.user
+
+        # Act
+        user = user_service.get_by_id(self.user_id, self.mock_db)
+
+        # Assert
+        self.assertEqual(user, self.user)
+        self.mock_db.query.assert_called_once_with(User)
+        assert_filter_called_with(query_mock, User.id == self.user_id)
+
+    def test_getById_returnsNone_whenUserIsNotFound(self) -> None:
+        # Arrange
+        query_mock = self.mock_db.query.return_value
+        filter_mock = query_mock.filter.return_value
+        filter_mock.first.return_value = None
+
+        # Act
+        user = user_service.get_by_id(self.user_id, self.mock_db)
+
+        # Assert
+        self.assertIsNone(user)
+        self.mock_db.query.assert_called_once_with(User)
+        assert_filter_called_with(query_mock, User.id == self.user_id)
