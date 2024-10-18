52d5dc7b81735f572a11cff418c68a9a03ad6bcc
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-10-19 01:50:33 +0300

    created verification methods for category access

---
+++ forum_system_api/services/utils/category_access_utils.py
@@ -0,0 +1,54 @@
+from uuid import UUID
+
+from fastapi import HTTPException, status
+from sqlalchemy.orm import Session
+
+from forum_system_api.persistence.models.access_level import AccessLevel
+from forum_system_api.persistence.models.topic import Topic
+from forum_system_api.persistence.models.user import User
+from forum_system_api.schemas.topic import TopicCreate
+from forum_system_api.services.category_service import get_by_id as get_category_by_id
+from forum_system_api.services.user_service import is_admin
+
+
+def user_permission(user: User, topic: Topic | TopicCreate, db: Session) -> bool:
+    category = get_category_by_id(category_id=topic.category_id, db=db)
+    if not category:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Category not found"
+        )
+
+    if category.is_locked:
+        return is_admin(user_id=user.id, db=db)
+
+    if category.is_private:
+        return category_permission(user=user, topic=topic, db=db)
+
+    return True
+
+
+def get_access_level(user: User, topic: Topic, db: Session) -> AccessLevel:
+    return next(
+        (
+            p.access_level
+            for p in user.permissions
+            if p.category_id == topic.category_id and p.user_id == user.id
+        ),
+        None,
+    )
+
+
+def category_permission(user: User, topic: Topic, db: Session) -> bool:
+    user_access = get_access_level(user=user, topic=topic, db=db)
+    return (
+        next(
+            (
+                p.category_id
+                for p in user.permissions
+                if p.category_id == topic.category_id
+            ),
+            None,
+        )
+        is not None
+        and user_access == AccessLevel.WRITE
+    ) or is_admin(user_id=user.id, db=db)
