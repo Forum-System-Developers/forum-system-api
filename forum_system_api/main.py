1dcafea0213b3896b39887cb178b317b3097e7f3
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-10-19 14:10:16 +0300

    react setup

--- forum_system_api/main.py
+++ forum_system_api/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from forum_system_api.api.api_v1.api import api_router
 from forum_system_api.persistence.database import create_tables
@@ -9,11 +10,14 @@ create_tables()
 app = FastAPI()
 
 app.include_router(api_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Frontend origin
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 
 if __name__ == "__main__":
     uvicorn.run("forum_system_api.main:app", host="127.0.0.1", port=8000, reload=True)
-
-# category id: 15fef349-8e42-44bb-a649-e30a422b4018
-# user id ed834a53-7e0b-47d0-ba0e-920840c70d40
-# admin  f6dce522-5006-4340-a8f3-ee66b1468b15
