3b55b7d6126a12bbb89d4d69c222831ce59ce5e0
Sph1nx <nikita.ryapok@proton.me>
2024-10-19 19:35:54 +0300

    Added code validation and refactoring - Validation added to create_message function

--- forum_system_api/services/message_service.py
+++ forum_system_api/services/message_service.py
@@ -1,12 +1,18 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 from forum_system_api.persistence.models.user import User
-from ..persistence.models.message import Message
-from ..persistence.models.conversation import Conversation
-from ..schemas.message import MessageCreate
+from forum_system_api.persistence.models.message import Message
+from forum_system_api.persistence.models.conversation import Conversation
+from forum_system_api.schemas.message import MessageCreate
 
 
 def send_message(db: Session, message_data: MessageCreate, user: User) -> Message:
+    receiver = db.query(User).filter(User.id == message_data.receiver_id).first()
+
+    if not receiver:
+        raise HTTPException(status_code=404, detail="receiver not found")
+
     conversation = db.query(Conversation).filter(
         ((Conversation.user1_id == user.id) & (Conversation.user2_id == message_data.receiver_id)) |
         ((Conversation.user1_id == message_data.receiver_id) & (Conversation.user2_id == user.id))
