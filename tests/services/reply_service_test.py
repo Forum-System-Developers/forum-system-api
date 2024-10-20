7598a17e031c20fa5af359b6f7ba953a922ca7a0
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-10-20 17:36:43 +0300

    created tests for get_by_id, update, and create

--- tests/services/reply_service_test.py
+++ tests/services/reply_service_test.py
@@ -8,10 +8,7 @@ from forum_system_api.persistence.models.category import Category
 from forum_system_api.persistence.models.reply import Reply
 from forum_system_api.persistence.models.topic import Topic
 from forum_system_api.persistence.models.user import User
-from forum_system_api.persistence.models.user_category_permission import (
-    UserCategoryPermission,
-)
-from forum_system_api.schemas.reply import ReplyCreate
+from forum_system_api.schemas.reply import ReplyCreate, ReplyUpdate
 from forum_system_api.services import reply_service
 from forum_system_api.services.user_service import is_admin
 from tests.services import test_data_const as tc
@@ -32,81 +29,111 @@ class ReplyServiceShould(unittest.TestCase):
         filter_mock = query_mock.filter.return_value
         filter_mock.first.return_value = self.reply
 
-        topic = reply_service.get_by_id(self.reply.id, self.user, self.db)
+        with patch('forum_system_api.services.reply_service.category_permission', return_value=True):
+            reply = reply_service.get_by_id(self.user, self.reply.id, self.db)
 
-        self.assertEqual(topic, self.reply)
+            self.assertEqual(reply, self.reply)
 
-        self.db.query.assert_called_once_with(Topic)
+            self.db.query.assert_called_once_with(Reply)
+
 
     def test_getById_noReply_returns404(self):
         query_mock = self.db.query.return_value
         filter_mock = query_mock.filter.return_value
         filter_mock.first.return_value = None
 
-        with self.assertRaises(HTTPException) as context:
-            reply_service.get_by_id(self.reply.id, self.db)
+        with patch('forum_system_api.services.reply_service.category_permission', return_value=True):
+            with self.assertRaises(HTTPException) as context:
+                reply_service.get_by_id(self.user, self.reply.id, self.db)
 
-        self.assertEqual(context.exception.status_code, 404)
-        self.assertEqual(context.exception.detail, "Reply not found")
+                self.assertEqual(context.exception.status_code, status.HTTP_404_NOT_FOUND)
+                self.assertEqual(context.exception.detail, "Reply not found")
+                self.db.query.assert_called_once_with(Reply)
 
-        self.db.query.assert_called_once_with(Reply)
 
-    def test_createReply_createsReply_validUserAccess(self):
-        reply_create = ReplyCreate(content=tc.VALID_REPLY_CONTENT)
 
-        with (
-            patch(
-                "forum_system_api.services.reply_service._validate_reply_access",
-                return_value=self.topic,
-            ),
-            patch(
-                "forum_system_api.services.reply_service.category_permission",
-                return_value=True,
-            ),
-        ):
-            new_reply = reply_service.create(
-                self.topic.id, reply_create, self.user, self.db
+    def test_getById_invalidCategoryPermission_raises403(self):
+        with patch(
+            "forum_system_api.services.reply_service.category_permission"
+        ) as category_permission_mock:
+            category_permission_mock.side_effect = HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Cannot fetch reply",
             )
+            with self.assertRaises(HTTPException) as context:
+                reply_service.get_by_id(self.user, self.reply.id, self.db)
 
-            self.db.add.assert_called_once_with(new_reply)
-            self.db.commit.assert_called_once()
-            self.db.refresh.assert_called_once_with(new_reply)
-            self.assertEqual(new_reply.topic_id, self.topic.id)
-            self.assertEqual(new_reply.author_id, self.user.id)
-            self.assertEqual(new_reply.content, tc.VALID_REPLY_CONTENT)
+            self.assertEqual(
+                context.exception.status_code, status.HTTP_403_FORBIDDEN
+            )
+            self.assertEqual(context.exception.detail, "Cannot fetch reply")            
 
-    def test_createReply_invalidReplyAccess_raises401(self):
+
+
+
+    def test_createReply_invalidReplyAccess_raises403(self):
         reply_create = ReplyCreate(content=tc.VALID_REPLY_CONTENT)
 
         with patch(
             "forum_system_api.services.reply_service._validate_reply_access"
         ) as valdate_reply_mock:
             valdate_reply_mock.side_effect = HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
+                status_code=status.HTTP_403_FORBIDDEN,
                 detail="Cannot reply to this post",
             )
             with self.assertRaises(HTTPException) as context:
                 reply_service.create(self.topic.id, reply_create, self.user, self.db)
 
             self.assertEqual(
-                context.exception.status_code, status.HTTP_401_UNAUTHORIZED
+                context.exception.status_code, status.HTTP_403_FORBIDDEN
             )
-            self.assertEqual(context.exception.detail, str(context.exception.detail))
-
-    def test_createReply_invalidCategoryPermission_raises401(self):
+            self.assertEqual(context.exception.detail, 'Cannot reply to this post')
+            
+            
+    def test_createReply_invalidCategoryPermission_raises403(self):
         reply_create = ReplyCreate(content=tc.VALID_REPLY_CONTENT)
 
         with patch(
             "forum_system_api.services.reply_service.category_permission"
-        ) as catwgory_permission_mock:
-            catwgory_permission_mock.side_effect = HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
+        ) as category_permission_mock:
+            category_permission_mock.side_effect = HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
                 detail="Cannot reply to this post",
             )
             with self.assertRaises(HTTPException) as context:
                 reply_service.create(self.topic.id, reply_create, self.user, self.db)
 
             self.assertEqual(
-                context.exception.status_code, status.HTTP_401_UNAUTHORIZED
+                context.exception.status_code, status.HTTP_403_FORBIDDEN
             )
-            self.assertEqual(context.exception.detail, str(context.exception.detail))
+            self.assertEqual(context.exception.detail, 'Cannot reply to this post')
+
+
+    def test_update_updatesReply_content_authorIsUser(self):
+        reply_update = ReplyUpdate(content=tc.VALID_REPLY_CONTENT_2)
+
+        self.reply.author_id = self.user.id
+        
+        with patch('forum_system_api.services.reply_service.get_by_id', return_value=self.reply):
+            updated_reply = reply_service.update(self.user, self.reply.id, reply_update, self.db)
+            
+            self.db.commit.assert_called_once()
+            self.db.refresh.assert_called_once_with(self.reply)
+            self.assertEqual(updated_reply.content, self.reply.content)
+            
+            
+
+    def test_update_authorIsNotUser_raises403(self):
+        reply_update = ReplyUpdate(content=tc.VALID_REPLY_CONTENT_2)
+
+        self.reply.author_id = tc.VALID_USER_ID_2
+        
+        with patch('forum_system_api.services.reply_service.get_by_id', return_value=self.reply):
+            
+            with self.assertRaises(HTTPException) as context:
+                reply_service.update(self.user, self.reply.id, reply_update, self.db)
+                
+                self.assertEqual(
+                context.exception.status_code, status.HTTP_403_FORBIDDEN
+            )
+                self.assertEqual(context.exception.detail, 'Cannot reply to this post')
