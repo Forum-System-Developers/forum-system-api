50362a0a7b96db186bb893341b89cc664171d324
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-10-20 17:35:48 +0300

    reply_router get_by_id refactored

--- forum_system_api/api/api_v1/routes/reply_router.py
+++ forum_system_api/api/api_v1/routes/reply_router.py
@@ -19,8 +19,12 @@ reply_router = APIRouter(prefix="/replies", tags=["replies"])
 
 
 @reply_router.get("/{reply_id}", response_model=ReplyResponse, status_code=200)
-def get_by_id(reply_id: UUID, db: Session = Depends(get_db)) -> ReplyResponse:
-    reply = reply_service.get_by_id(reply_id=reply_id, db=db)
+def get_by_id(
+    reply_id: UUID,
+    user: User = Depends(get_current_user),
+    db: Session = Depends(get_db)
+) -> ReplyResponse:
+    reply = reply_service.get_by_id(user=user, reply_id=reply_id, db=db)
     votes = reply_service.get_votes(reply=reply)
     return ReplyResponse.create(reply=reply, votes=votes)
 
