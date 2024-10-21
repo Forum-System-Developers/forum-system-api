a943971aa46254bb22e15848193b663acb6692ac
Sph1nx <nikita.ryapok@proton.me>
2024-10-21 10:10:49 +0300

    upd message sending logic to use         get_or_create_conversation
    Updated send_message function to use the newly
    created get_or_create_conversation for handling
    conversation retrieval or creation, streamlining
    the process of sending messages.

--- forum_system_api/services/message_service.py
+++ forum_system_api/services/message_service.py
@@ -7,22 +7,28 @@ from forum_system_api.persistence.models.conversation import Conversation
 from forum_system_api.schemas.message import MessageCreate
 
 
+def get_or_create_conversation(db: Session, user_id: int, receiver_id: int) -> Conversation:
+    conversation = db.query(Conversation).filter(
+        ((Conversation.user1_id == user_id) & (Conversation.user2_id == receiver_id)) |
+        ((Conversation.user1_id == receiver_id) & (Conversation.user2_id == user_id))
+    ).first()
+
+    if not conversation:
+        conversation = Conversation(user1_id=user_id, user2_id=receiver_id)
+        db.add(conversation)
+        db.commit()
+        db.refresh(conversation)
+
+    return conversation
+
+
 def send_message(db: Session, message_data: MessageCreate, user: User) -> Message:
     receiver = db.query(User).filter(User.id == message_data.receiver_id).first()
 
     if not receiver:
-        raise HTTPException(status_code=404, detail="receiver not found")
+        raise HTTPException(status_code=404, detail="Receiver not found")
 
-    conversation = db.query(Conversation).filter(
-        ((Conversation.user1_id == user.id) & (Conversation.user2_id == message_data.receiver_id)) |
-        ((Conversation.user1_id == message_data.receiver_id) & (Conversation.user2_id == user.id))
-    ).first()
-
-    if not conversation:
-        conversation = Conversation(user1_id=user.id, user2_id=message_data.receiver_id)
-        db.add(conversation)
-        db.commit()
-        db.refresh(conversation)
+    conversation = get_or_create_conversation(db, user.id, message_data.receiver_id)
 
     message = Message(content=message_data.content, conversation_id=conversation.id, author_id=user.id)
     db.add(message)
