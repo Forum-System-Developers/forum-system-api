2a91d7c14a8dad9d566c7b9177289da1b46b0f8a
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-10-22 15:31:21 +0300

    feat: add test for register_user endpoint in user_router_test.py
    - Added unit test for user registration endpoint
    - Verified HTTP 201 Created status code

---
+++ tests/routes/user_router_test.py
@@ -0,0 +1,38 @@
+import unittest
+from unittest.mock import MagicMock, patch
+
+from sqlalchemy.orm import Session
+from fastapi import status
+from fastapi.testclient import TestClient
+
+from forum_system_api.main import app
+from forum_system_api.persistence.database import get_db
+from forum_system_api.persistence.models.user import User
+from forum_system_api.api.api_v1.constants import endpoints as e
+from tests.services import test_data as td
+
+
+client = TestClient(app)
+
+class TestUserRouter_Should(unittest.TestCase):
+    def setUp(self) -> None:
+        self.mock_db = MagicMock(spec=Session)
+        self.mock_admin = MagicMock(spec=User)
+        self.user = MagicMock(spec=User, **td.USER_1)
+        self.user2 = MagicMock(spec=User, **td.USER_2)
+    
+    def tearDown(self) -> None:
+        app.dependency_overrides = {}
+
+    @patch('forum_system_api.services.user_service.create')
+    def test_registerUser_returns200_onSuccess(self, mock_create) -> None:
+        # Arrange
+        mock_create.return_value = self.user
+        app.dependency_overrides[get_db] = lambda: self.mock_db
+
+        # Act
+        response = client.post(e.USERS_REGISTER_ENDPOINT, json=td.USER_CREATE)
+        
+        # Assert
+        self.assertIsInstance(response.json(), dict)
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
