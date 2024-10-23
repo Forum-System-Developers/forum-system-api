1de2ef29b23521aa804cbd49530ba2878af4b61e
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-10-23 17:24:57 +0300

    refactored topic_rotuer

--- forum_system_api/api/api_v1/routes/topic_router.py
+++ forum_system_api/api/api_v1/routes/topic_router.py
@@ -1,30 +1,33 @@
+from typing import Literal
 from uuid import UUID
 
-from fastapi import APIRouter, HTTPException, status
-from fastapi import Depends
+from fastapi import APIRouter, Depends, HTTPException, Query, status
 from sqlalchemy.orm import Session
 
 from forum_system_api.persistence.database import get_db
+from forum_system_api.persistence.models.user import User
 from forum_system_api.schemas.common import TopicFilterParams
 from forum_system_api.schemas.topic import (
-    TopicResponse,
     TopicCreate,
+    TopicResponse,
     TopicUpdate,
-    TopicLock,
 )
-from forum_system_api.persistence.models.user import User
-from forum_system_api.services import topic_service, category_service
+from forum_system_api.services import topic_service
 from forum_system_api.services.auth_service import get_current_user, require_admin_role
-
 
 topic_router = APIRouter(prefix="/topics", tags=["topics"])
 
 
-@topic_router.get("/", response_model=list[TopicResponse], status_code=200)
+@topic_router.get(
+    "/",
+    response_model=list[TopicResponse],
+    status_code=200,
+    description="Get a list of all topics along with their replies",
+)
 def get_all(
-    filter_query: TopicFilterParams = Depends(), 
+    filter_query: TopicFilterParams = Depends(),
     db=Depends(get_db),
-    user: User = Depends(get_current_user)
+    user: User = Depends(get_current_user),
 ) -> list[TopicResponse]:
     topics = topic_service.get_all(filter_params=filter_query, user=user, db=db)
     return [
@@ -36,11 +39,16 @@ def get_all(
     ]
 
 
-@topic_router.get("/{topic_id}", response_model=TopicResponse, status_code=200)
+@topic_router.get(
+    "/{topic_id}",
+    response_model=TopicResponse,
+    status_code=200,
+    description="Get a topic by its ID along with its replies",
+)
 def get_by_id(
-    topic_id: UUID, 
+    topic_id: UUID,
     db: Session = Depends(get_db),
-    user: User = Depends(get_current_user)
+    user: User = Depends(get_current_user),
 ) -> TopicResponse:
     topic = topic_service.get_by_id(topic_id=topic_id, user=user, db=db)
     return TopicResponse.create(
@@ -49,7 +57,9 @@ def get_by_id(
     )
 
 
-@topic_router.post("/", response_model=TopicResponse, status_code=201)
+@topic_router.post(
+    "/", response_model=TopicResponse, status_code=201, description="Create a new topic"
+)
 def create(
     topic: TopicCreate,
     user: User = Depends(get_current_user),
@@ -59,7 +69,12 @@ def create(
     return TopicResponse.create(topic=topic, replies=[])
 
 
-@topic_router.put("/{topic_id}", response_model=TopicResponse, status_code=201)
+@topic_router.put(
+    "/{topic_id}",
+    response_model=TopicResponse,
+    status_code=201,
+    description="Update a topic. You're able to update the title, or assign it to a different category",
+)
 def update(
     topic_id: UUID,
     updated_topic: TopicUpdate,
@@ -75,19 +90,28 @@ def update(
     )
 
 
-@topic_router.put("/{topic_id}/locked", status_code=201)
+@topic_router.put(
+    "/{topic_id}/locked",
+    status_code=201,
+    description="Admin can Lock or Unlock a topic",
+)
 def lock(
     topic_id: UUID,
-    lock_topic: TopicLock,
+    lock_topic: bool = Query(default=True),
     admin: User = Depends(require_admin_role),
     db: Session = Depends(get_db),
 ) -> dict:
-    topic = topic_service.lock(topic_id=topic_id, lock_topic=lock_topic, db=db)
-    return {"msg": "Topic locked"} if topic.is_locked else {"msg": "Unlocked"}
+    topic = topic_service.lock(
+        user=admin, topic_id=topic_id, lock_topic=lock_topic, db=db
+    )
+    return {"msg": "Topic locked"} if topic.is_locked else {"msg": "Topic unlocked"}
 
 
 @topic_router.put(
-    "/{topic_id}/replies/{reply_id}/best", response_model=TopicResponse, status_code=201
+    "/{topic_id}/replies/{reply_id}/best",
+    response_model=TopicResponse,
+    status_code=201,
+    description="Select the best reply for a topic. Only the author of the topic can select the best reply",
 )
 def best_reply(
     topic_id: UUID,
