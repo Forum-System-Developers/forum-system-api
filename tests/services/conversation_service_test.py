1110add44e0dcb2663370372080dd536180a9913
Sph1nx <nikita.ryapok@proton.me>
2024-10-24 01:42:05 +0300

    test: add unit test to cover user1_id case in get_users_from_conversations

--- tests/services/conversation_service_test.py
+++ tests/services/conversation_service_test.py
@@ -13,7 +13,7 @@ from forum_system_api.services.conversation_service import (
 from forum_system_api.persistence.models.user import User
 from forum_system_api.persistence.models.message import Message
 from forum_system_api.persistence.models.conversation import Conversation
-from tests.services.test_data import USER_1
+from tests.services.test_data import USER_1, USER_2
 from tests.services.utils import assert_filter_called_with
 
 class TestConversationService(unittest.TestCase):
@@ -21,6 +21,7 @@ class TestConversationService(unittest.TestCase):
     def setUp(self) -> None:
         self.db = MagicMock()
         self.user = User(**USER_1)
+        self.user_2= User(**USER_2)
         self.conversation_id = uuid4()
         self.conversation = Conversation(id=self.conversation_id, user1_id=self.user.id, user2_id=uuid4())
         self.message1 = Message(id=uuid4(), conversation_id=self.conversation_id, content="Hello")
@@ -90,16 +91,24 @@ class TestConversationService(unittest.TestCase):
     @patch('forum_system_api.services.conversation_service.get_conversations_for_user')
     def test_get_users_from_conversations_found(self, mock_get_conversations_for_user) -> None:
         # Arrange
-        mock_get_conversations_for_user.return_value = [self.conversation]
-        self.db.query.return_value.filter.return_value.all.return_value = [self.user]
+        conversation1 = Conversation(id=self.conversation_id, user1_id=self.user.id, user2_id=uuid4())
+        conversation2 = Conversation(id=uuid4(), user1_id=uuid4(), user2_id=self.user.id)
+
+        mock_get_conversations_for_user.return_value = [conversation1, conversation2]
+
+        user1 = self.user
+        user2 = self.user_2
+        self.db.query.return_value.filter.return_value.all.return_value = [user1, user2]
 
         # Act
         users = get_users_from_conversations(self.db, self.user)
 
         # Assert
-        self.assertEqual(len(users), 1)
-        self.assertEqual(users[0].id, self.user.id)
-        expected_filter = User.id.in_({self.conversation.user2_id})
+        self.assertEqual(len(users), 2)
+        self.assertIn(user1, users)
+        self.assertIn(user2, users)
+
+        expected_filter = User.id.in_({conversation1.user2_id, conversation2.user1_id})
         assert_filter_called_with(self.db.query.return_value, expected_filter)
 
     @patch('forum_system_api.services.conversation_service.get_conversations_for_user')
