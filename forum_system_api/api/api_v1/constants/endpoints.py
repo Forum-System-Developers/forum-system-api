ca02b5547494bb048dc230c5cc0bd717e19ef8fe
Sph1nx <nikita.ryapok@proton.me>
2024-10-23 22:53:32 +0300

    Add const endpoints for conversation

--- forum_system_api/api/api_v1/constants/endpoints.py
+++ forum_system_api/api/api_v1/constants/endpoints.py
@@ -17,3 +17,6 @@ CATEGORY_PRIVACY_ENDPOINT = "/api/v1/categories/{}/private/"
 CATEGORY_LOCK_ENDPOINT = "/api/v1/categories/{}/lock/"
 
 MESSAGE_ENDPOINT_SEND_MESSAGE = "/api/v1/messages/"
+
+CONVERSATION_MESSAGES_ENDPOINT = "/api/v1/conversations/{}/"
+CONVERSATION_CONTACTS_ENDPOINT = "/api/v1/conversations/{}/contacts"
