3167a5a9ef1316915faf84aaa1798d030145eec8
Sph1nx <nikita.ryapok@proton.me>
2024-10-23 19:44:00 +0300

    Add unit test for viewing category

--- tests/routes/category_router_test.py
+++ tests/routes/category_router_test.py
@@ -6,10 +6,10 @@ from forum_system_api.main import app
 from forum_system_api.persistence.database import get_db
 from forum_system_api.persistence.models.topic import Topic
 from forum_system_api.persistence.models.user import User
-from forum_system_api.services.auth_service import  require_admin_role
+from forum_system_api.services.auth_service import get_current_user, require_admin_role
 from forum_system_api.api.api_v1.constants import endpoints as e
 from tests.services import test_data as td
-from tests.services.test_data_obj import USER_1
+from tests.services.test_data_obj import USER_1, VALID_REPLY, VALID_TOPIC_1
 
 client = TestClient(app)
 
@@ -48,3 +48,19 @@ class TestCategoryRouter_Should(unittest.TestCase):
         # Assert
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json(), list)
+
+    @patch('forum_system_api.services.topic_service.get_all')
+    @patch('forum_system_api.services.topic_service.get_replies')
+    def test_view_category_returns200_onSuccess(self, mock_get_replies, mock_get_all) -> None:
+        # Arrange
+        topic_instance = Topic(id=VALID_TOPIC_1["id"], category_id=VALID_TOPIC_1["category_id"])
+        mock_get_all.return_value = [topic_instance]
+        mock_get_replies.return_value = [VALID_REPLY]
+        app.dependency_overrides[get_db] = lambda: self.mock_db
+        app.dependency_overrides[get_current_user] = lambda: self.user
+
+        # Act
+        response = client.get(e.CATEGORY_ENDPOINT_VIEW_CATEGORY.format(td.VALID_CATEGORY_ID))
+        
+        # Assert
+        self.assertEqual(response.status_code, 200)
