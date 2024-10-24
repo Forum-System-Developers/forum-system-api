45521f546944feb7e83fcd6d6ba2a9963c2a10b9
Sph1nx <nikita.ryapok@proton.me>
2024-10-24 16:28:08 +0300

    feat: add docstrings in category_service

--- forum_system_api/services/category_service.py
+++ forum_system_api/services/category_service.py
@@ -8,6 +8,15 @@ from forum_system_api.schemas.category import CreateCategory, CategoryResponse
 
 
 def create_category(data: CreateCategory, db: Session) -> CategoryResponse:
+    """
+    Creates a new category in the database.
+
+    Args:
+        data (CreateCategory): The data required to create a new category.
+        db (Session): The database session used to interact with the database.
+    Returns:
+        CategoryResponse: The newly created category.
+    """
     new_category = Category(**data.model_dump())
     
     db.add(new_category)
@@ -18,6 +27,16 @@ def create_category(data: CreateCategory, db: Session) -> CategoryResponse:
 
 
 def get_all(db: Session) -> list[CategoryResponse]:
+    """
+    Retrieve all categories from the database.
+
+    Args:
+        db (Session): The database session.
+    Returns:
+        list[CategoryResponse]: A list of CategoryResponse objects representing all categories.
+    Raises:
+        HTTPException: If no categories are found in the database.
+    """
     categories = db.query(Category).all()
 
     if not categories:
@@ -39,6 +58,16 @@ def get_all(db: Session) -> list[CategoryResponse]:
 
 
 def get_by_id(category_id: UUID, db: Session) -> Category:
+    """
+    Retrieve a category by its ID.
+
+    Args:
+        category_id (UUID): The unique identifier of the category.
+        db (Session): The database session used for querying.
+
+    Returns:
+        Category: The category object if found, otherwise None.
+    """
     return (db.query(Category)
                 .filter(Category.id == category_id)
                 .first())
@@ -49,6 +78,18 @@ def make_private_or_public(
         is_private: bool, 
         db: Session
 ) -> Category:
+    """
+    Update the privacy status of a category.
+
+    Args:
+        category_id (UUID): The unique identifier of the category.
+        is_private (bool): The desired privacy status of the category.
+        db (Session): The database session to use for the operation.
+    Returns:
+        Category: The updated category object.
+    Raises:
+        HTTPException: If the category with the given ID is not found.
+    """
     category = get_by_id(category_id, db)
 
     if category is None:
@@ -66,6 +107,18 @@ def lock_or_unlock(
         is_locked: bool, 
         db: Session
 ) -> Category:
+    """
+    Lock or unlock a category based on the provided category ID.
+    
+    Args:
+        category_id (UUID): The unique identifier of the category to be locked or unlocked.
+        is_locked (bool): A boolean indicating whether to lock (True) or unlock (False) the category.
+        db (Session): The database session used to perform the operation.
+    Returns:
+        Category: The updated category object.
+    Raises:
+        HTTPException: If the category with the given ID is not found.
+    """
     category = get_by_id(category_id, db)
 
     if category is None:
