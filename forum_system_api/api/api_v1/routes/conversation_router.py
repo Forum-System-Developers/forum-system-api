e4d4758fa2d7c5e8b8b17d80d8c9352e51af44f1
Sph1nx <nikita.ryapok@proton.me>
2024-10-25 01:06:21 +0300

    refactor: Add detailed descriptions to conversation_router.py endpoints

--- forum_system_api/api/api_v1/routes/conversation_router.py
+++ forum_system_api/api/api_v1/routes/conversation_router.py
@@ -17,7 +17,7 @@ from forum_system_api.services.auth_service import get_current_user
 conversation_router = APIRouter(prefix="/conversations", tags=["conversations"])
 
 
-@conversation_router.get("/{conversation_id}", response_model=list[MessageResponse])
+@conversation_router.get("/{conversation_id}", response_model=list[MessageResponse], description="Read messages in a conversation")
 def read_messages_in_conversation(
     conversation_id: UUID,
     user: User = Depends(get_current_user),
@@ -26,7 +26,7 @@ def read_messages_in_conversation(
     return get_messages_in_conversation(db, conversation_id)
 
 
-@conversation_router.get("/{user}/contacts", response_model=list[UserResponse])
+@conversation_router.get("/{user}/contacts", response_model=list[UserResponse], description="Get all users with whom the current user has conversations")
 def get_users_with_conversations_route(
     user: User = Depends(get_current_user), 
     db: Session = Depends(get_db)
